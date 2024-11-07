9d50061fc7d37fe5af31badb2c8b7131e1edd31b
mhafezan <mohamad.hafezan@gmail.com>
2024-11-07 14:37:25 -0500

    Black-Box attack updated for substitute Model3

--- Models/CIFAR100_Model3/1_train_model3/train_test_model3.py
+++ Models/CIFAR100_Model3/1_train_model3/train_test_model3.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import StepLR
 from models.model3 import Model3
 
-def train(model, train_loader, test_loader, optimizer, device, args):
+def train(model, train_loader, test_loader, optimizer, scheduler, device, args):
     
     for epoch in range(args.epochs):
         model.train()
@@ -33,9 +33,6 @@ def train(model, train_loader, test_loader, optimizer, device, args):
             # Loss over the batch progress (Printing the loss every x batches)
             if (index + 1) % args.print_freq == 0:
                 print(f'Epoch [{epoch+1}/{args.epochs}], Step [{index+1}/{len(train_loader)}], Loss: {loss.item():.4f}', '\n')
-        
-        # Adjust the learning rate
-        scheduler.step()
 
         # Average loss over the epoch progress (Printing the loss after each epoch execution)
         print(f'Epoch [{epoch+1}/{args.epochs}] ********** Average Loss: {running_loss/len(train_loader):.4f} **********', '\n')
@@ -55,8 +52,15 @@ def train(model, train_loader, test_loader, optimizer, device, args):
         accuracy = float(correct / processed_images) * 100
         print(f'Epoch [{epoch+1}/{args.epochs}] ********** Test Accuracy: {accuracy:.2f}% *********', '\n')
 
+        if args.optimizer == 'sgd':
+            scheduler.step()
+        else:
+            scheduler.step(accuracy)
+            current_lr = scheduler.optimizer.param_groups[0]['lr']
+            print(f"Learning Rate after Epoch [{epoch+1}]: {current_lr:.6f}\n")
+
         # Save the model parameters after each epoch
-        if accuracy >= 70:
+        if accuracy >= 40:
             model_save_path = f'{args.weights}/model2_epoch_{epoch+1}_{args.lr}_{args.batch_size}_{accuracy:.2f}.pth'
             torch.save(model.state_dict(), model_save_path)
 
@@ -83,20 +87,20 @@ def test(model, test_loader, device, model_path):
 
     return
 
-"""def initialize_weights(model):
+def initialize_weights(model):
     # Initializes the weights of the given model. Uses Kaiming Initialization for CONV layers and Xavier Initialization for FC layers
     
     for layer in model.modules():
-        if isinstance(layer, nn.Conv2d):
+        """if isinstance(layer, nn.Conv2d):
             # Kaiming Initialization for convolutional layers
             nn.init.kaiming_normal_(layer.weight, mode='fan_out', nonlinearity='relu')
             if layer.bias is not None:
-                nn.init.constant_(layer.bias, 0)
-        elif isinstance(layer, nn.Linear):
+                nn.init.constant_(layer.bias, 0)"""
+        if isinstance(layer, nn.Linear):
             # Xavier Initialization for fully connected layers
             nn.init.xavier_normal_(layer.weight)
             if layer.bias is not None:
-                nn.init.constant_(layer.bias, 0)"""
+                nn.init.constant_(layer.bias, 0)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='CIFAR100 Training on substitute Model3')
@@ -104,7 +108,7 @@ if __name__ == "__main__":
     parser.add_argument('--weights', default="./weights", help="The path to store model parameters")
     parser.add_argument('--epochs', default=100, type=int, metavar='N', help='Number of total epochs to train')
     parser.add_argument('--batch-size', default=128, type=int, metavar='N', help='Mini-batch size')
-    parser.add_argument('--lr', default=0.001, type=float, metavar='LR', help='Learning rate')
+    parser.add_argument('--lr', default=0.01, type=float, metavar='LR', help='Learning rate')
     parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
     parser.add_argument('--optimizer', default="sgd", help="Optimizer option is either adam or sgd")
     parser.add_argument('--print_freq', default=100, type=int, help="To print loss and accuracy statistics every X mini-batch")
@@ -127,13 +131,15 @@ if __name__ == "__main__":
     # Model Initialization
     model = Model3().to(device)
 
-    """# Initialize weights
-    model.apply(initialize_weights)"""
+    # Initialize weights
+    model.apply(initialize_weights)
     
     # CIFAR100 dataset and dataloader declaration
     CIFAR100_MEAN = (0.5070751592371323, 0.48654887331495095, 0.4409178433670343)
     CIFAR100_STD  = (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)
-    TRANSFORM = transforms.Compose([transforms.ToTensor(), transforms.Normalize(CIFAR100_MEAN, CIFAR100_STD)])
+    TRANSFORM = transforms.Compose([transforms.ToTensor(),
+                                    transforms.RandomHorizontalFlip(),
+                                    transforms.Normalize(CIFAR100_MEAN, CIFAR100_STD)])
 
     train_dataset = torchvision.datasets.CIFAR100(root=args.dataset, train=True, download=True, transform=TRANSFORM)
     test_dataset = torchvision.datasets.CIFAR100(root=args.dataset, train=False, download=True, transform=TRANSFORM)
@@ -146,16 +152,20 @@ if __name__ == "__main__":
 
     if args.optimizer == 'sgd':
         optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-    else:
+        scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
+    elif args.optimizer == 'adam':
         optimizer = optim.Adam(model.parameters(), lr=args.lr)
-        scheduler = StepLR(optimizer, step_size=30, gamma=0.1) # Reduce LR by a factor of 0.1 every 30 epochs
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=5)
+    else:
+        optimizer = optim.AdamW(model.parameters(), lr=args.lr)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=5)
 
     if args.phase == "train":
-        train(model, train_loader, test_loader, optimizer, device, args)
+        train(model, train_loader, test_loader, optimizer, scheduler, device, args)
     else:
         test(model, test_loader, device, args.model_path)
 
     sys.exit(0)
 
-# python3 train_test_model3.py --phase train --epochs 100 --lr 0.001 --batch-size 128 --optimizer adam
+# python3 train_test_model3.py --phase train --epochs 100 --lr 0.01 --batch-size 256 --print_freq 20 --optimizer sgd
 # python3 train_test_model3.py --phase test --batch-size 128 --model_path ./weights/model3_epoch_6_0.001_64_70.69.pth
